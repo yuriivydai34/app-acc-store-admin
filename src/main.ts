b3e5db921ec475f6a1c3751feb612f9d3594db10
Yurii Vydai <yuriivydai@gmail.com>
2025-06-09 12:54:38 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Store example')
     .setDescription('The store API description')
